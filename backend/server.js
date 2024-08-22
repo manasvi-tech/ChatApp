16df2f3dcc3f35ba64003562d0388a226e26041a
manasvi-tech <manasviarora28@gmail.com>
2024-08-22 23:12:01 +0530

    frontend and backend working with npm start

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,29 @@ import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
+import path from "path"
 
 import connectToMongo from "./db/connect.js";
 import { app,server } from "./socket/socket.js"
 
 dotenv.config();
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.get("/", (req,res)=>{
-    res.send("Hello kids");
-})
 
 app.use("/api/auth", authRoutes );
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend" , "dist" , "index.html"))
+})
 
 server.listen(PORT, ()=>{
     connectToMongo();
