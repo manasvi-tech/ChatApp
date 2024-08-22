3793ee7fb774e5b2bb1b09213a63e6774bd67bcf
manasvi-tech <manasviarora28@gmail.com>
2024-08-22 23:23:08 +0530

    changed node_env

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateTokenAndSetCookie = (userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:15*24*60*60*1000, //ms
         httpOnly: true, //prevents XSS attacks cross-site scripting attacks
-        sameSite:"strict" //CSRF attacks cross-site request forgery attacks
+        sameSite:"strict", //CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
     })
 }
 
